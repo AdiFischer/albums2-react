8783724f7454f2c0bd17f3e16964370b113365bb
AdiFischer <adifischer1@gmail.com>
2022-11-02 12:14:20 -0400

    adding New Album Form

--- src/scenes/AddAlbum.jsx
+++ src/scenes/AddAlbum.jsx
@@ -1,8 +1,10 @@
 import Header from "../components/Header"
+import NewAlbumForm from "../components/NewAlbumForm"
 export default function AddAlbum() {
     return(
         <>
         <Header title={'Add new Album'} />
+        <NewAlbumForm />
         <p>New Album Form</p>
         </>
     )
